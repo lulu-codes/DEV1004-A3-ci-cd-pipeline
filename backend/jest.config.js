7cf21c6ce5640e581e93fbe8331f3f0f52b936c0
Nhi Huynh <159687209+lulu-codes@users.noreply.github.com>
2026-02-21 12:51:18 +0800

    fix: add leading slash to test path ignore patterns as tests not detecting in runner

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -4,7 +4,7 @@ export default {
   // Exclude node_modules from testing
   coveragePathIgnorePatterns: ['/node_modules/'],
   // Exclude specific test setup files from being treated as tests
-  testPathIgnorePatterns: ['src/__tests__/setup/'],
+  testPathIgnorePatterns: ['/src/__tests__/setup/'],
   // Stops jest using transformers on JS files (e.g. Babel)
   transform: {},
   // Maps module paths to avoid issues with ES6 module imports
